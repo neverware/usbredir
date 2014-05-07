d88de229d2b25802c8726ef800e2ce171d27878e
dgs3 <sayles.dave@gmail.com>
2014-05-07 08:39:56 -0400

    Added build script for client machines to easily build

---
+++ build_usbredir.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Grab dependencies
+sudo apt-get install autoconf libtool
+
+PREFIX=/opt/neverware_usbredir
+if [ ! -d $PREFIX ]; then
+    mkdir -p $PREFIX
+fi
+./autogen.sh --prefix=$PREFIX
+make
+sudo make install
